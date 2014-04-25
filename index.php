169b888c7a134cb3ca0f51e1edfa03fc72ef4b3a
Mediatrends <mauro@mediatrends.cl>
2014-04-25 16:44:45 -0300

    udpate 25/04 front login page

--- index.php
+++ index.php
@@ -11,40 +11,7 @@
 
 </head>
 <body>
-	<div class="container">
-		<div class="row">
-			<div class="logo_mediatrends">
-				
-				<div class="img_logo">
-					<img src="img/logo_mt.gif" alt="">
-				</div>
-				<div class="text-tit">
-					<h1>
-						MediaKit<br><span>Review</span>
-					</h1>
-				</div>
-
-			</div>
-		</div>
-		<div class="row">
-			<div class="form_access">
-				
-				<form action="">
-					
-					<div class="user_row">
-						<label for="user">Usuario:</label>
-						<input type="text">
-					</div>
-					<div class="pass_row">
-						<label for="pass">Contraseña:</label>
-						<input type="text">
-					</div>
-					
-					<button class="btn_enter">entrar</button>
-				</form>
-
-			</div>
-		</div>
-	</div>
+	<!--?php include 'login.php' ?-->
+	<?php include 'category.php' ?>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,35 @@
+<div class="container_log">
+	<div class="row">
+		<div class="logo_mediatrends">
+			
+			<div class="img_logo">
+				<img src="img/logo_mt.gif" alt="">
+			</div>
+			<div class="text-tit">
+				<h1>
+					MediaKit<br><span>Review</span>
+				</h1>
+			</div>
+
+		</div>
+	</div>
+	<div class="row">
+		<div class="form_access">
+			
+			<form action="">
+				
+				<div class="user_row">
+					<label for="user">Usuario:</label>
+					<input type="text">
+				</div>
+				<div class="pass_row">
+					<label for="pass">Contraseña:</label>
+					<input type="text">
+				</div>
+				
+				<button class="btn_enter">entrar</button>
+			</form>
+
+		</div>
+	</div>
+</div>
