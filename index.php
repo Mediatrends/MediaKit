f73f88317075cfacb2e3e5dec29df9e7f9ed4d8c
Mediatrends <mauro@mediatrends.cl>
2014-04-25 16:05:03 -0300

    udpate 25/04 front login page

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Media kit</title>
+
+	<link rel="stylesheet" href="css/main.css">
+	<!--[if (gte IE 6)&(lte IE 9)]>
+		<script src="js/vendor/respond.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="logo_mediatrends">
+				
+				<div class="img_logo">
+					<img src="img/logo_mt.gif" alt="">
+				</div>
+				<div class="text-tit">
+					<h1>
+						MediaKit<br><span>Review</span>
+					</h1>
+				</div>
+
+			</div>
+		</div>
+		<div class="row">
+			<div class="form_access">
+				
+				<form action="">
+					
+					<div class="user_row">
+						<label for="user">Usuario:</label>
+						<input type="text">
+					</div>
+					<div class="pass_row">
+						<label for="pass">Contraseña:</label>
+						<input type="text">
+					</div>
+					
+					<button class="btn_enter">entrar</button>
+				</form>
+
+			</div>
+		</div>
+	</div>
+</body>
+</html>
